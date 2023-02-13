1be4742eac5a4cbb31be0ed764eca970cf2dbf65
shehroz4477 <shehrozaaa@gmail.com>
2023-02-14 02:51:53 +0500

    remove extra middile ware from middle controkller

--- serverSite/Program.cs
+++ serverSite/Program.cs
@@ -10,15 +10,15 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
