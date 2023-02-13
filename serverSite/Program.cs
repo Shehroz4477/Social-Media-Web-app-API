7d2d9ef8fadf537ba35cc9266067e5a96e79eb30
shehroz4477 <shehrozaaa@gmail.com>
2023-02-14 02:45:26 +0500

    comment unused servives from service container

--- serverSite/Program.cs
+++ serverSite/Program.cs
@@ -4,8 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
