1b73e0647b3f9850df0e7745eb862a095643ccf8
shehroz4477 <shehrozaaa@gmail.com>
2023-02-15 00:34:12 +0500

    create entityframe work class

---
+++ serverSite/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using serverSite.Entities;
+
+namespace serverSite.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
