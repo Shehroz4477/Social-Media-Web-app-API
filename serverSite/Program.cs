1957a5f65c42689a86408b63115b35db7ecfd094
shehroz4477 <shehrozaaa@gmail.com>
2023-02-15 21:25:10 +0500

    define comments for services container and middleware

--- serverSite/Program.cs
+++ serverSite/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//  Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DbContext>((DbContextOptionsBuilder context) => context.UseSqlite(builder.Configuration.GetConnectionString("socialMediaSqliteConnection")));
 
 var app = builder.Build();
 
+//  Configure the HTTP request pipeline
 app.MapControllers();
 
 app.Run();
