b1bdb8f1f0ca92c99dc75be6ec027e1a809570b2
shehroz4477 <shehrozaaa@gmail.com>
2023-02-19 23:59:25 +0500

    add corse policy for client side project

--- serverSite/Program.cs
+++ serverSite/Program.cs
@@ -6,10 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 //  Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>((DbContextOptionsBuilder context) => context.UseSqlite(builder.Configuration.GetConnectionString("socialMediaSqliteConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 //  Configure the HTTP request pipeline
+app.UseCors(configurePolicy => configurePolicy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
