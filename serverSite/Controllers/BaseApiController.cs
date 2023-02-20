512cff7a89e24a149a718b406dafc166f772a6f7
shehroz4477 <shehrozaaa@gmail.com>
2023-02-20 23:35:08 +0500

    baseApiCintroller create

---
+++ serverSite/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace serverSite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/controllerNmae
+    public class BaseApiController:ControllerBase
+    {
+        
+    }
+}
