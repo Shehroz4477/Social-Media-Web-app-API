af81173cc70e6f523b20c8375b59e2325de19c87
shehroz4477 <shehrozaaa@gmail.com>
2023-02-20 22:57:34 +0500

    AppUser Entity update

--- serverSite/Entities/AppUser.cs
+++ serverSite/Entities/AppUser.cs
@@ -4,9 +4,11 @@ namespace serverSite.Entities
 {
     public class AppUser
     {
-        // specify the primary key option
-        [Key] 
+        [Key] // specify the primary key option
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
