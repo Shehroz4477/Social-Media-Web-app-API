441efbfa03930527fbea671c24b418138a9e9e93
shehroz4477 <shehrozaaa@gmail.com>
2023-02-22 02:51:29 +0500

    adding login end point

--- serverSite/Controllers/AccountController.cs
+++ serverSite/Controllers/AccountController.cs
@@ -17,7 +17,8 @@ namespace serverSite.Controllers
         }
 
         [HttpPost("register")] // Post: /api/account/register....
-        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO){
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
 
             if(await this.IsUserExist(_context,registerDTO.UserName)) return BadRequest("User Already Exist");
 
@@ -35,5 +36,28 @@ namespace serverSite.Controllers
 
             return user;
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await this.GetUser(_context, loginDTO.UserName);
+            if(!this._UserAuthorized(user,loginDTO)) return Unauthorized("Invalid User Name or Password");
+            return user;
+        }
+
+        // private methods
+        private bool _UserAuthorized(AppUser user, LoginDTO loginDTO){
+            if(user == null) return false;
+            var hmac = new HMACSHA512(user.PasswordSalt);
+            var passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+            for (int i = 0; i < passwordHash.Length; i++)
+            {
+                if(passwordHash[i] != user.PasswordHash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        } 
     }
 }
