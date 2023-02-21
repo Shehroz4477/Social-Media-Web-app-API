ffb7e0ac4d34d8797771e4a7914f0cce06ed4cb1
shehroz4477 <shehrozaaa@gmail.com>
2023-02-22 03:16:44 +0500

    Add token service

---
+++ serverSite/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using serverSite.Entities;
+using serverSite.Interfaces;
+
+namespace serverSite.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
