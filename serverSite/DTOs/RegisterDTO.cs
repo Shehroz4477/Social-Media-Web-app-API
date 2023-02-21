09621cd6bfac4c43b310d3371287a9c0bdf89a0f
shehroz4477 <shehrozaaa@gmail.com>
2023-02-22 01:31:39 +0500

    Adding validation

--- serverSite/DTOs/RegisterDTO.cs
+++ serverSite/DTOs/RegisterDTO.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace serverSite.DTOs
 {
     public class RegisterDTO
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
