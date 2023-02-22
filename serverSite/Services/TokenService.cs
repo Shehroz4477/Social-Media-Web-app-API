736f7bb8dc17c730c815fd1779bdb1a52df48f5a
shehroz4477 <shehrozaaa@gmail.com>
2023-02-23 02:53:12 +0500

    create JWT token create

--- serverSite/Services/TokenService.cs
+++ serverSite/Services/TokenService.cs
@@ -1,13 +1,59 @@
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using serverSite.Entities;
 using serverSite.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace serverSite.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+            };
+
+            var cred = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = new DateTime().AddDays(2),
+                SigningCredentials = cred
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+         public string CreateToken(AppUser user,string tokenKey)
+        {
+            var securityToken = new JwtSecurityTokenHandler().CreateToken
+            (
+                new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(
+                        new List<Claim>
+                        {
+                            new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+                        }
+                    ),
+                    Expires = new DateTime().AddDays(2),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                        SecurityAlgorithms.HmacSha512Signature
+                    )
+                }
+            );
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
         }
     }
 }
