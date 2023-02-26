26b36017e580e0d3576d1913d230f28357f72916
shehroz4477 <shehrozaaa@gmail.com>
2023-02-26 23:09:19 +0500

    application service extension define

---
+++ serverSite/Extensions/AppServiceExtension.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using serverSite.Data;
+using serverSite.Interfaces;
+using serverSite.Services;
+
+namespace serverSite.Extensions
+{
+    public static class AppServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllers();
+            services.AddDbContext<DataContext>((DbContextOptionsBuilder context) => context.UseSqlite(config.GetConnectionString("socialMediaSqliteConnection")));
+            services.AddCors();
+            services.AddScoped<ITokenService,TokenService>();
+
+            return services;
+        }
+    }
+}
