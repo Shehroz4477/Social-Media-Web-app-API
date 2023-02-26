ffaf16ec18e4175bdf488679beb1c2306cf68847
shehroz4477 <shehrozaaa@gmail.com>
2023-02-26 20:09:15 +0500

    userDTO

---
+++ serverSite/DTOs/UserDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace serverSite.DTOs
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+}
