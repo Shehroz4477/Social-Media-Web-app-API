4d91d62220a30de70a8ace965c346777a6175c41
shehroz4477 <shehrozaaa@gmail.com>
2023-03-02 00:38:31 +0500

    fully defined relationship with appUser entity

--- serverSite/Entities/Photo.cs
+++ serverSite/Entities/Photo.cs
@@ -6,5 +6,8 @@ namespace serverSite.Entities
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
