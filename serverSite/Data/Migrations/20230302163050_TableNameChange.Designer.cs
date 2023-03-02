a31558d0b0f32ca5655e96ef71d3257e07da951d
shehroz4477 <shehrozaaa@gmail.com>
2023-03-02 22:24:31 +0500

    table name change and update ef migration

--- serverSite/Data/Migrations/20230302163050_TableNameChange.Designer.cs
+++ serverSite/Data/Migrations/20230302163050_TableNameChange.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using serverSite.Data;
 
@@ -10,9 +11,11 @@ using serverSite.Data;
 namespace serverSite.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230302163050_TableNameChange")]
+    partial class TableNameChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.1");
