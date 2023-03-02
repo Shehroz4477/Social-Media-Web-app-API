e218438f5ca3b9d13c3c73211fc2b87b2ccd1db2
shehroz4477 <shehrozaaa@gmail.com>
2023-03-03 02:32:00 +0500

    create member and photo DTO class

---
+++ serverSite/DTOs/MemberDTO.cs
@@ -0,0 +1,19 @@
+namespace serverSite.DTOs
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<PhotoDTO> Photos { get; set; }
+    }
+}
---
+++ serverSite/DTOs/PhotoDTO.cs
@@ -0,0 +1,9 @@
+namespace serverSite.DTOs
+{
+    public class PhotoDTO
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
