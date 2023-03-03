88f7cf7ebf4fc6992e28cd14a66f2900b1678c30
shehroz4477 <shehrozaaa@gmail.com>
2023-03-04 00:17:28 +0500

    add automapper package

--- serverSite/Extensions/AppServiceExtension.cs
+++ serverSite/Extensions/AppServiceExtension.cs
@@ -14,6 +14,7 @@ namespace serverSite.Extensions
             services.AddCors();
             services.AddScoped<ITokenService,TokenService>();
             services.AddScoped<IUserRepository,UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
