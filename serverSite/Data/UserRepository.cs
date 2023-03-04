1c24a9b709b2e180c93c8598cd1966c1e491fec7
shehroz4477 <shehrozaaa@gmail.com>
2023-03-04 13:36:34 +0500

    UserRepository=> implements new methods to return Member DTO

--- serverSite/Data/UserRepository.cs
+++ serverSite/Data/UserRepository.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using serverSite.DTOs;
 using serverSite.Entities;
 using serverSite.Interfaces;
 
@@ -7,11 +10,36 @@ namespace serverSite.Data
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
-        public UserRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
-            
         }
+
+        public async Task<MemberDTO> GetMemberByIdAsync(int id)
+        {
+            return await _context.Users
+                .Where(user => user.Id == id)
+                .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<MemberDTO> GetMemberByNameAsync(string userName)
+        {
+            return await _context.Users
+                .Where(user => user.UserName == userName)
+                .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<MemberDTO>> GetMembersAsync()
+        {
+            return await _context.Users
+                .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
         public async Task<AppUser> GetUserByIdAsync(int id)
         {
             return await _context.Users.FindAsync(id);
