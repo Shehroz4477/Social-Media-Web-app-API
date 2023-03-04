98b4c0010205d09d1d6f04449632a811e9f3ef8c
shehroz4477 <shehrozaaa@gmail.com>
2023-03-04 13:35:40 +0500

    IUserRepository => add new method to return memberDTO

--- serverSite/Interfaces/IUserRepository.cs
+++ serverSite/Interfaces/IUserRepository.cs
@@ -1,3 +1,4 @@
+using serverSite.DTOs;
 using serverSite.Entities;
 
 namespace serverSite.Interfaces
@@ -7,6 +8,9 @@ namespace serverSite.Interfaces
         public Task<IEnumerable<AppUser>> GetUsersAsync();
         public Task<AppUser> GetUserByIdAsync(int id);
         public Task<AppUser> GetUserByNameAsync(string userName);
+        public Task<IEnumerable<MemberDTO>> GetMembersAsync();
+        public Task<MemberDTO> GetMemberByIdAsync(int id);
+        public Task<MemberDTO> GetMemberByNameAsync(string userName);
         public Task<bool> SaveAllAsync();
         public void Update(AppUser user);
 
